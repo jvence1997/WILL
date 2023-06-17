f7fddcb4e49271292de9b9668c5a15c837565b8f
jairo vence mattias <jairo_vencemattias@hotmail.com>
2023-06-17 01:53:29 -0300

    Ultimo terminado

--- 09.js
+++ 09.js
@@ -4,6 +4,7 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
+
 function filtrar(funcion) {
   // Escribi una función filtrar en el prototipo de Arrays,
   // que recibe una funcion (callback) que devuelve true o false.
@@ -24,6 +25,16 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
+  
+  Array.prototype.filtrar = function(callback) {
+    let nuevoArr = [];
+    this.forEach(data => {
+        if(callback(data)) {
+            nuevoArr.push(data);
+        }
+    });
+    return nuevoArr;
+  };
 
 };
 
